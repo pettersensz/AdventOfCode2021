5f576a60deaa2aefe1ca4b1c6027f2b513b67b36
pettersensz <pettersen.sigmund@gmail.com>
2022-08-26 20:59:23 +0200

    Day 1 Part 1

--- AdventOfCode2021.Cmd/Program.cs
+++ AdventOfCode2021.Cmd/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AdventOfCode2021.Cmd
 {
@@ -6,7 +7,33 @@ namespace AdventOfCode2021.Cmd
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      // Day 1
+      DetermineNumberOfIncreases();
+      Console.WriteLine("The end...");
+    }
+
+    private static void DetermineNumberOfIncreases()
+    {
+      var file = File.ReadAllLines(@"C:\kode\AdventOfCode2021\day1\day1_test_input.txt");
+      //var file = File.ReadAllLines(@"C:\kode\AdventOfCode2021\day1\day1_input.txt");
+      var lineCounter = 0;
+      var increaseCounter = 0;
+      var currentValue = 0;
+      var previousValue = 0;
+      foreach(var line in file)
+      {
+        lineCounter++;
+        int.TryParse(line, out currentValue);
+        if (lineCounter != 1)
+        {
+          if (currentValue > previousValue) increaseCounter++;
+        }
+
+        previousValue = currentValue;
+      }
+
+      Console.WriteLine(lineCounter + " lines were read");
+      Console.WriteLine(increaseCounter + " increases were found");
     }
   }
 }
