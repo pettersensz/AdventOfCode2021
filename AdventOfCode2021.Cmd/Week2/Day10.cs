608675b8958dbcbfee23bbe3ee2d6a39ea0ccc67
Sigmund Pettersen <pettersen.sigmund@gmail.com>
2022-09-03 20:07:29 +0200

    Continue day 10

--- AdventOfCode2021.Cmd/Week2/Day10.cs
+++ AdventOfCode2021.Cmd/Week2/Day10.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode2021.Cmd.Week2
 {
   public class Day10
   {
+    private char[] _allowedOpeningChars = new char[] { '(', '[', '{', '<' };
+    private char[] _allowedClosingChars = new char[] { ')', ']', '}', '>' };
+
     public Day10(string fileName)
     {
-      var allowedOpeningChars = new char[] { '(', '[', '{', '<' };
-      var allowedClosingChars = new char[] { ')', ']', '}', '>' };
       var data = Common.ReadFile.ReadLinesInTextFile(fileName);
       for (var i = 0; i < 1; i++)
       {
-        var line = data[i];
-        var chunkStack = new Stack();
-
+        var line = data[i].ToCharArray();
+        var chunkStack = new Stack<char>();
+        foreach (var c in line)
+        {
+          if (_allowedOpeningChars.Contains(c))
+          {
+            chunkStack.Push(c);
+          }
+          else if (_allowedClosingChars.Contains(c))
+          {
+            var openingChar = chunkStack.Pop();
+            var indexOpening = Array.IndexOf(openingChar, )
+          }
+        }
       }
     }
   }
